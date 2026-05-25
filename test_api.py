4f8c3875ddb2a018a2522176c463a5fcc45038d3
Ryan <2822016383@qq.com>
2026-05-25 16:22:28 +0800

    local model

--- test_api.py
+++ test_api.py
@@ -4,8 +4,8 @@ from langchain_openai import ChatOpenAI
 
 # 配置 DeepSeek 模型
 llm = ChatOpenAI(
-    model="deepseek-chat",  # 模型名称固定
-    base_url="https://api.deepseek.com/v1",  # 官方接口地址
+    model="qwen3.5:0.8b",  # 模型名称固定
+    base_url="http://localhost:11434/v1",  # 官方接口地址
     temperature=0.5,
 )
 
@@ -13,6 +13,4 @@ llm = ChatOpenAI(
 response = llm.invoke("你好，请介绍一下自己")
 print(response.content)
 
-test
-hot test
-remote test
+
